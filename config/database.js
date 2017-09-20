eeb73faa2827a76b68662c6007c2d98ed355c3d1
Praveen Saboji <praveensbj8@gmail.com>
2017-09-20 01:51:54 -0400

    Update database.js
    changing DB config value

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database : 'mongodb://praveen:praveen@ds021711.mlab.com:21711/praveendb',
+    database : 'mongodb://localhost:21711/praveendb',
     secret : 'yoursecret'
 }
